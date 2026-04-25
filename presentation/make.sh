48d398cc8e1c65f9d2369308c19812d1ae753494
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-04-25 12:23:19 +0200

    Improvements

--- presentation/make.sh
+++ presentation/make.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-npx @marp-team/marp-cli presentation.md -o presentation.html
+BASE_DIR=`dirname $0`
+INPUT="${BASE_DIR}/presentation.md"
+OUTPUT="${BASE_DIR}/presentation.html"
+
+npx @marp-team/marp-cli ${INPUT} -o ${OUTPUT}
